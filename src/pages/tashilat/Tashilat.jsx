ae3af6d24b6e3fcf8ef7ab724826891d98955f58
Ali Rough <ali.hastam2015@gmail.com>
2023-06-07 22:46:32 +0330

    confirm allert

--- src/pages/tashilat/Tashilat.jsx
+++ src/pages/tashilat/Tashilat.jsx
@@ -1,7 +1,8 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation, useParams } from "react-router-dom";
 
 export default function Tashilat() {
+  let { pathname: pass } = useLocation();
   return (
     <div className="px-5">
       <div className=" py-6">
@@ -50,19 +51,18 @@ export default function Tashilat() {
         </div>
         <div className="flex items-center">
           <p className="bg-blue-200 p-0.5 pt-1 px-3 rounded-xl text-blue-800 ">
-            تاییدیه
+            تایید
           </p>
         </div>
-
       </div>
       <Outlet />
-
-      <div className=" text-left mt-2">
-        <button className="bg-blue-700  text-white rounded-xl p-4 font-bold text-sm">
-          
-          مرحله بعد
-        </button>
-      </div>
+      {pass == !"/panel/Tashilat/confirm" && (
+        <div className=" text-left mt-2">
+          <button className="bg-blue-700  text-white rounded-xl p-4 font-bold text-sm">
+            مرحله بعد
+          </button>
+        </div>
+      )}
     </div>
   );
 }
